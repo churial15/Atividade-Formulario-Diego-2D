7dec8ed27089fb4c711220a62d63053ac27c174c
DiegoGabrielLL <diego.lazzarin@escola.pr.gov.br>
2025-09-05 07:58:24 -0300

    Update conexao.php

--- Projeto-Diego-2d/conexao.php
+++ Projeto-Diego-2d/conexao.php
@@ -4,15 +4,16 @@
 $host    = "localhost";   // normalmente não precisa alterar
 $usuario = "root";        // substituir se seu usuário não for root
 $senha   = "";            // substituir se você tiver senha no MySQL
-$banco   = "projeto-diego-2d";       // substituir pelo nome do seu banco criado no phpMyAdmin
+$banco   ="projeto-diego-2d";       // substituir pelo nome do seu banco criado no phpMyAdmin
 
 // Conexão MySQLi
 $conexao = mysqli_connect($host, $usuario, $senha, $banco);
 
+//Verificação de erro
 if (!$conexao) {
     die("Erro ao conectar: " . mysqli_connect_error());
 }
 
 // SENSITIVE CASE suportar acentos e Ç
-mysqli_set_charset($conexao, "utf8mb");
+mysqli_set_charset($conexao, "utf8");
 ?>
